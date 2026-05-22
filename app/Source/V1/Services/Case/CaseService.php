29ac50cae45d6a7e40524ad80c245c444b3a6ab7
Maciej Samotyj <maciej.samotyj-lenczewski@e-instytucja.pl>
2026-05-22 10:01:54 +0200

    #63002 - normalizacja tekstu - interesanci

--- app/Source/V1/Services/Case/CaseService.php
+++ app/Source/V1/Services/Case/CaseService.php
@@ -155,6 +155,13 @@ class CaseService
                 foreach ($row['pozostali_interesanci'] as &$interesant) {
                     $interesant['interesant'] = Functions::normalizeText($interesant['interesant']);
                     $interesant['interesant_adres'] = Functions::normalizeText($interesant['interesant_adres']);
+                    $row['pozostali_interesanci_tooltip'] =
+                        $interesant['interesant']
+                        . (
+                        !empty($interesant['interesant_adres'])
+                            ? '<br>' . $interesant['interesant_adres']
+                            : ''
+                        );
                 }
             }
 
