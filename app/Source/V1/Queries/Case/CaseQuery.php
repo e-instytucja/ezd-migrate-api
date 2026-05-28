07c493813a96bcfec1e866b24bf8a77ef83a91f6
Maciej Samotyj <maciej.samotyj-lenczewski@e-instytucja.pl>
2026-05-28 15:40:35 +0200

    #67376 - poprawka zliczania i paginacji.

--- app/Source/V1/Queries/Case/CaseQuery.php
+++ app/Source/V1/Queries/Case/CaseQuery.php
@@ -185,7 +185,10 @@ WHERE eo.status_sprawy_id = r.status_sprawy_id;
                 --         ON (fd_tytul.sprawa_uid = es.sprawa_uid AND fd_tytul.form_dane_pole = 'dokument_tytul')
                 LEFT JOIN eurzad_form_dane fd_petent
                        ON (fd_petent.sprawa_uid = es.sprawa_uid AND fd_petent.form_dane_pole = 'petent_uid')
-                LEFT JOIN eurzad_petent_dane pd_petent ON (pd_petent.main_petent_uid = fd_petent.form_dane_wartosc)
+                LEFT JOIN eurzad_petent_dane pd_petent ON (
+                        pd_petent.main_petent_uid = fd_petent.form_dane_wartosc 
+                        AND pd_petent.petent_uid = pd_petent.main_petent_uid
+                )
                 LEFT JOIN eurzad_petent_search ps_petent ON (ps_petent.main_petent_uid = fd_petent.form_dane_wartosc)
                 LEFT JOIN eurzad_form_dane fd_pliki
                        ON (fd_pliki.sprawa_uid = es.sprawa_uid AND fd_pliki.form_dane_pole = 'pliki')
